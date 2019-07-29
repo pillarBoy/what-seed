1acce20588db7eafbb4b5ba943281955da628e52
issolf <issolf@guangpu.info>
2019-07-29 18:26:33 +0800

    style:remove accidently include file

--- src/test/src/index.js
+++
@@ -1,8 +0,0 @@
-import "./assets/index.scss";
-import aa from './index.ts'
-import { log } from '%/devUtils.js'
-
-console.log(aa);
-log()
-
-console.log('bitkan webpack pc seed');
--- src/test/src/utils/htmlFontSize.js
+++
@@ -1,8 +0,0 @@
-function px2rem(px) {
-  return px * 2 / 10 * window.rem + 'rem;'
-}
-(function setHtmlFontSize() {
-  let fs = document.documentElement.clientWidth / 10;
-  window.rem = fs;
-  document.querySelector('html').style = `font-size:${fs}px`;
-}())
