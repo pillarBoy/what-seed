53daab347219f5709a7dd9cfdf8fc24f8d0e3978
pillar <601991736@qq.com>
2019-08-01 18:42:31 +0800

    webpack项目优化

--- webpack-seed/webpack/webpack.dev.config.js
+++ webpack-seed/webpack/webpack.dev.config.js
@@ -18,20 +18,6 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
     new FriendlyErrorsWebpackPlugin(),
   ],
-  module: {
-    rules: [
-      {
-        test: /\.(s?css)$/,
-        use: [
-          { loader: "style-loader" }, // 将 JS 字符串生成为 style 节点)
-          { loader: "css-loader", }, // 将 CSS 转化成 CommonJS 模块
-          { loader: 'px2rem-loader' , options: { remUni: 75, remPrecision: 8 } }, // px to rem
-          { loader: 'postcss-loader', options: { sourceMap: true } },
-          { loader: "sass-loader" }, // 将 Sass 编译成 CSS
-        ]
-      }
-    ]
-  },
   devtool: "cheap-module-eval-source-map",
   devServer: {
     // contentBase: 'public',
--- webpack-seed/webpack/webpack.production.config.js
+++ webpack-seed/webpack/webpack.production.config.js
@@ -1,5 +1,4 @@
 var path = require('path')
-// var copyWepackPlugin = require('copy-webpack-plugin')
 var MiniCssExtractPlugin = require("mini-css-extract-plugin")
 var ManifestPlugin = require('webpack-manifest-plugin')
 var CleanWebpackPlugin = require('clean-webpack-plugin')
@@ -10,20 +9,6 @@ var config = require('./config')
 
 var webpackModule = {
   mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.(s?css)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          { loader: "css-loader", }, // 将 CSS 转化成 CommonJS 模块
-          { loader: 'px2rem-loader', options: { remUni: 75, remPrecision: 8 } }, // px to rem
-          { loader: 'postcss-loader', options: { sourceMap: true } }, // auto perfixe
-          { loader: "sass-loader" }, // 将 Sass 编译成 CSS
-        ]
-      }
-    ]
-  },
   devtool: "cheap-module-source-map",
   optimization: {
     // minimize: true,
@@ -46,17 +31,7 @@ var webpackModule = {
     new MiniCssExtractPlugin({
       filename: `css/[name].[hash${config.hashLength ? ':' + config.hashLength : ''}].css`,
       chunkFilename: "[id].css"
-    }),
-    // new copyWepackPlugin([{
-    //   from: path.resolve(__dirname, '../public', '**/*'),
-    //   to: path.resolve(__dirname, '../dist'),
-    //   // flatten 路径
-    //   transformPath(targetPath) {
-    //     return targetPath.replace('public', '')
-    //   }
-    // }], {
-    //   ignore: ['*.html', '*.ejs']
-    // })
+    })
   ],
 }
 
